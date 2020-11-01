c1c3977f4a5bbde9ccf854e4f851537f58956e2f
Raynon Messias <raymessias@gmail.com>
2020-11-01 02:12:35 -0300

    commit: Created template

---
+++ src/app.js
@@ -0,0 +1,23 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+// ES6 Class
+class App extends Component {
+  render() {
+    return (
+      <fragment>
+        <div>Aplicação</div>
+        <div>Aplicação 2</div>
+      </fragment>
+    )
+  }
+}
+// Pure Function
+// const App = React.createClass({
+//   render: function () {
+//     return <div>Aplicação 2</div>
+//   }
+// })
+
+export default App
